a44090fff184357f0434fef0851176e5a4fa7a81
Satyender Yadav <satyenderyadav4993@gmail.com>
2023-12-23 02:16:24 +0530

    Entity Ongoing

--- src/components/FlowEditor/FlowEditor.tsx
+++ src/components/FlowEditor/FlowEditor.tsx
@@ -1,11 +1,11 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import ReactFlow, {
     addEdge,
     MiniMap,
     Controls,
     Background,
     useNodesState,
-    useEdgesState,
+    useEdgesState, ReactFlowInstance,
 } from 'reactflow';
 
 import { nodes as initialNodes, edges as initialEdges } from './InitialElements';
@@ -24,13 +24,18 @@ const minimapStyle = {
     height: 120,
 };
 
-const onInit = (reactFlowInstance: any) => console.log('flow loaded:', reactFlowInstance);
-
 const FlowEditor = () => {
+    const reactFlowWrapper = useRef<HTMLDivElement>(null);
     const proOptions = { hideAttribution: true };
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+    const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance | null>(null);
     const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), []);
+
+    const onInit = (reactFlowInstance: ReactFlowInstance) => {
+        // console.log('flow loaded:', reactFlowInstance);
+        setReactFlowInstance(reactFlowInstance);
+    };
 
     // TODO: Migrate code to entity editor too & use react flow panels to put add entity button.
 
@@ -50,24 +55,52 @@ const FlowEditor = () => {
         backgroundColor: "#171727"
     }
 
+    const onPaneContextMenu = (event: any) => {
+        event.preventDefault();
+
+        if (!reactFlowInstance || !reactFlowWrapper.current) return;
+
+        const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+
+        const relativePositionX = event.clientX - reactFlowBounds.left;
+        const relativePositionY = event.clientY - reactFlowBounds.top;
+
+        const viewportPoint = reactFlowInstance.project({
+            x: relativePositionX,
+            y: relativePositionY,
+        });
+
+        const newNode = {
+            id: Date.now().toString(), // or any unique ID
+            data: { label: 'New Node' },
+            position: viewportPoint,
+            // other node properties
+        };
+
+        setNodes((nds) => nds.concat(newNode));
+    };
+
     return (
-        <ReactFlow
-            style={flowStyle}
-            nodes={nodes}
-            edges={edgesWithUpdatedTypes}
-            onNodesChange={onNodesChange}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-            onInit={onInit}
-            fitView
-            proOptions={proOptions}
-            nodeTypes={nodeTypes}
-            maxZoom={1}
-        >
-            <MiniMap style={minimapStyle} zoomable pannable />
-            <Controls />
-            <Background color="#2B2B38" gap={16} size={3} />
-        </ReactFlow>
+        <div ref={reactFlowWrapper} style={{width: '100%', height: '100%'}}>
+            <ReactFlow
+                style={flowStyle}
+                nodes={nodes}
+                edges={edgesWithUpdatedTypes}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                onInit={onInit}
+                onPaneContextMenu={onPaneContextMenu}
+                fitView
+                proOptions={proOptions}
+                nodeTypes={nodeTypes}
+                maxZoom={1}
+            >
+                <MiniMap style={minimapStyle} zoomable pannable />
+                <Controls />
+                <Background color="#2B2B38" gap={16} size={3} />
+            </ReactFlow>
+        </div>
     );
 }
 
